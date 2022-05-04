0c336b2a9ff6bdb2865f494c2719d3c96d5587a3
Anup2799 <103236525+Anup2799@users.noreply.github.com>
2022-05-04 15:14:29 +0530

    Online-Store Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <h4 class="text-center"><b>(Computer & Hardware)</b></h4>
+        
+       <hr />
+      </div>
+    </div>
+   <!-- <app-products></app-products> -->
+    <router-outlet></router-outlet>
+  </div>
+     
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'app';
+cart: any[] =[];
+
+//cart: Observable<Array<any>> 
+  constructor(private store: Store<any>) {}
+
+  
+
+  ngOnInit() {
+    this.store.select('cart').subscribe((State => this.cart = State))  }
+}
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { PRODUCTS } from "./../store/market";
+
+
+@Component({
+  selector: 'app-products',
+  template: `<div class="container">
+  <div class= "row">
+        <div class="col-lg-4 col-md-4 col-sm-8 col-xs-16" *ngFor="let product of products">
+            <div class="my-list">
+                <img src="http://hpservicecenterschennai.in/images/hp_laptop_service_centers_in_guindy.png" alt="" />
+                <h3>{{product.name}}</h3>
+                <span>$</span>
+                <span class="pull-right">{{product.price}}</span>
+                <div class="offer"><b><u><marquee>Extra 5% Off</marquee></u></b></div>
+                <div class="detail">
+                    <p>{{product.name}} </p>
+                    <img src="http://hpservicecenterschennai.in/images/hp_laptop_service_centers_in_guindy.png" alt="" />
+                    <a [routerLink]="['/product',product.id]" class="btn btn-info"><b>View</b></a>
+                </div>
+            </div>  
+            </div>  
+            </div> 
+  `,
+  styles: [ ]
+})
+export class ProductsComponent implements OnInit {
+
+  products = PRODUCTS
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
